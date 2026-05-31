79d34ac10aecc4e1dbab1ec8fda3953d61e78248
Angela Andrews <aandrews17@yahoo.com>
2026-05-31 18:34:28 -0400

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-00e801948462f718a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "terraform-example"
+  }
+}
+
+
+
