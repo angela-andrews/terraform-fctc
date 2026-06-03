17622399692c831ef8524a4a69bf5d0fb0c3d942
Angela Andrews <38594234+angela-andrews@users.noreply.github.com>
2026-06-03 16:50:28 -0400

    starting over with ubuntu

--- main.tf
+++ main.tf
@@ -1,15 +1,55 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-00e801948462f718a"
+resource "aws_launch_configuration" "example" {
+  ami           = "ami-0fb653ca2d3203ac1"
   instance_type = "t2.micro"
+  security_groups = [aws_security_group.instance.id]
 
-  tags = {
-    Name = "terraform-example"
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, FCTC index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
+
+ # Required when using a launch configuration with an auto scaling group.
+  lifecycle {
+    create_before_destroy = true
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
 
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key = "Name"
+    value = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+variable "server_port" {
+  description = "The port the server will use for http requests"
+  type = number
+  default = 8080
+}
+
+output  "public_ip" {
+  value = aws_instance.example.public_ip
+  description = "The public IP address of the web server"
+}
